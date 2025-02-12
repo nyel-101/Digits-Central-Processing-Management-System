6f4d69ca51f85e6e56af5de8013868d2ea3044b0
nyel-101 <dandevjr@gmail.com>
2025-02-12 20:13:57 +0000

    fix(api): restructure payload parser

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25098c9ec7a542c196128c06c0dcff84 = () => {
-  return '2026-04-12T20:01:04Z';
+const node_da68723bdbc143208d389420d74aa9c2 = () => {
+  return '2025-02-12T20:13:57Z';
 };
