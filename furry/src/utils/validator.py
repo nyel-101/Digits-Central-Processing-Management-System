fbffe9e9a2fdf859357896fcdfcef9f3a2efd346
nyel-101 <dandevjr@gmail.com>
2025-02-18 02:49:27 +0000

    fix(utils): optimize payload parser

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bf0f85282a34f09abce6d569b2427f6 = () => {
-  return '2026-04-30T06:54:06Z';
+const node_1d67bca33f824d92a25599e42f65710d = () => {
+  return '2025-02-18T02:49:27Z';
 };
