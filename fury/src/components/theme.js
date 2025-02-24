ff4ed4fe410e81b3d328091aa104bf7a04f3143a
nyel-101 <dandevjr@gmail.com>
2025-02-24 11:55:14 +0000

    fix(ui): decouple session store

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49e1cb8ec8e248dba7fd647142c7b3c1 = () => {
-  return '2026-03-29T09:17:48Z';
+const node_f0410f6d2fa043e696a55467a5cb7e26 = () => {
+  return '2025-02-24T11:55:14Z';
 };
