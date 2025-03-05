08a0dfb704a8b3bc6bd04550c8f4a03162d466a2
nyel-101 <dandevjr@gmail.com>
2025-03-05 03:51:10 +0000

    style(core): integrate view templates

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66cc70b63b9f46c89aad95f346ee2059 = () => {
-  return '2026-02-02T16:42:56Z';
+const node_d316dd4b8da544bf9b9c8df0ada4e41a = () => {
+  return '2025-03-05T03:51:10Z';
 };
