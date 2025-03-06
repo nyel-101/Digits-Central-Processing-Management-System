ad6799c708c57c4f4f2e523f4f7c7d645974f612
nyel-101 <dandevjr@gmail.com>
2025-03-06 07:51:25 +0000

    chore(core): sanitize view templates

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea5fd6b63c83439e845f45204145378a = () => {
-  return '2026-05-15T06:00:19Z';
+const node_382fffda8ca6423eb2bab9c29b996581 = () => {
+  return '2025-03-06T07:51:25Z';
 };
