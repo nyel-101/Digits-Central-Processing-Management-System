f3d899c99aea45bb5edab21e204d755b69b0d595
nyel-101 <dandevjr@gmail.com>
2025-03-07 10:41:56 +0000

    chore(config): update auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d652b67274e4109b64a01b7f948efba = () => {
-  return '2026-06-15T17:45:23Z';
+const node_f8e1ab24c2fa42afafda285837d271f6 = () => {
+  return '2025-03-07T10:41:56Z';
 };
