66c4e42bba5ff7bc7689fa9c0e815565b718be35
nyel-101 <dandevjr@gmail.com>
2025-03-07 22:42:29 +0000

    fix(auth): restructure view templates

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99a3e37bb7cf498cbdfb49144a1e172c = () => {
-  return '2025-02-20T16:28:16Z';
+const node_cf4fc6016d364a0eb68d99e6656b45a6 = () => {
+  return '2025-03-07T22:42:29Z';
 };
