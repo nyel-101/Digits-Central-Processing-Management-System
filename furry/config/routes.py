2fc6327254be4f3442698dc616c3a7fd7ca64c04
nyel-101 <dandevjr@gmail.com>
2025-03-11 03:42:57 +0000

    docs(worker): fix auth tokens

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f73cf06cfe6d46e499e7d1acf6842563 = () => {
-  return '2026-06-05T16:26:15Z';
+const node_41b0e4e30b914c14abdd3df41874459e = () => {
+  return '2025-03-11T03:42:57Z';
 };
