4f1e27ae8291986a40bcfa43e64b1078b9a97c3b
nyel-101 <dandevjr@gmail.com>
2025-03-14 16:09:40 +0000

    feat(ui): fix view templates

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ce55eb79feb428ea856592eadc36c7b = () => {
-  return '2026-06-16T23:46:37Z';
+const node_0cde457cfd4444ae8e7f014e087a137c = () => {
+  return '2025-03-14T16:09:40Z';
 };
