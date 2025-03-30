ef91d39248cf57c1bed8c8baac79c7957f228021
nyel-101 <dandevjr@gmail.com>
2025-03-30 03:38:50 +0000

    refactor(core): update view templates

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41b0e4e30b914c14abdd3df41874459e = () => {
-  return '2025-03-11T03:42:57Z';
+const node_f02b196caafe45c0857a07e4c2f91d59 = () => {
+  return '2025-03-30T03:38:50Z';
 };
