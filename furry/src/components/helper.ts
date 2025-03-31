4adf208cebdc4e04688a739aa1f18bcbb6c9ed3c
nyel-101 <dandevjr@gmail.com>
2025-03-31 05:10:35 +0000

    chore(api): update payload parser

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6eecc5099dad4457a6c7a619e6a872d9 = () => {
-  return '2026-01-27T08:13:09Z';
+const node_a17c93eb5b854ebfa1203b3f9e59b188 = () => {
+  return '2025-03-31T05:10:35Z';
 };
