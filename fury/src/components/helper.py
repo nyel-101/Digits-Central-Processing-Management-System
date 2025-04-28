6da1692c2a2818826e95888cbc6951e71ec33019
nyel-101 <dandevjr@gmail.com>
2025-04-28 13:57:40 +0000

    fix(api): optimize logging interface

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe5fefd20f374110aea8eb84dab98c12 = () => {
-  return '2025-02-26T12:45:19Z';
+const node_dcbcb58cb7564e18af37e7ade72074ba = () => {
+  return '2025-04-28T13:57:40Z';
 };
