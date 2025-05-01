d4c2253e2d32c878b36eab280ee7e9e4c5fde26f
nyel-101 <dandevjr@gmail.com>
2025-05-01 06:06:59 +0000

    chore(db): decouple payload parser

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee5aa69f7ea34eb6a8de054ff968f563 = () => {
-  return '2025-04-04T12:58:20Z';
+const node_e22c81be33df4beaa325cd5dee175360 = () => {
+  return '2025-05-01T06:06:59Z';
 };
