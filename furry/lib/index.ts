3544a76037d29360ec2848d5780a628ff5aa0f5b
nyel-101 <dandevjr@gmail.com>
2025-05-23 04:02:43 +0000

    fix(db): fix payload parser

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f261813074645e69f93bbd5100b01ef = () => {
-  return '2025-04-22T12:18:58Z';
+const node_08e4115a0f934e439b77b7cd22d5fd60 = () => {
+  return '2025-05-23T04:02:43Z';
 };
