cd49a8e3667ae360e527d9f1156f55eaa0f322f1
nyel-101 <dandevjr@gmail.com>
2025-06-06 22:09:58 +0000

    refactor(api): fix auth tokens

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25b6f6046d164409af4f6bec86fe27a8 = () => {
-  return '2025-04-08T02:03:06Z';
+const node_3eb2c5871e7e479b889f0cd0887b17fe = () => {
+  return '2025-06-06T22:09:58Z';
 };
