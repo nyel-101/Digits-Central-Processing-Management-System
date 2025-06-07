af338b467dc423e5c1476b4d191e6a06996fed47
nyel-101 <dandevjr@gmail.com>
2025-06-07 19:42:56 +0000

    fix(auth): restructure session store

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_113cf6f4af48450ab3e95bac1f821215 = () => {
-  return '2025-05-08T17:43:17Z';
+const node_106085f9f6c04c5d94fb1068bdd27c4b = () => {
+  return '2025-06-07T19:42:56Z';
 };
