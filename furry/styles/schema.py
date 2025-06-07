389127524600fa6243fba6f6ef2c801ae29ee171
nyel-101 <dandevjr@gmail.com>
2025-06-07 11:32:41 +0000

    test(config): update payload parser

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f6c797dd0b549c8a6a5c6ad644f687c = () => {
-  return '2025-04-30T11:34:34Z';
+const node_aaf1e2d6e6974deaafe2de2f81e067e8 = () => {
+  return '2025-06-07T11:32:41Z';
 };
