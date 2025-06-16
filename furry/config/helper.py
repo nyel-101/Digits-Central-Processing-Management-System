3cb45ebca030bbdf24596bc9adbee7bdae8e743b
nyel-101 <dandevjr@gmail.com>
2025-06-16 12:17:25 +0000

    style(ui): enhance auth tokens

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08f018b882d64f2eaa0ac2c8313be7b2 = () => {
-  return '2026-04-24T01:28:42Z';
+const node_31519073bd5f441e97713d79537895d4 = () => {
+  return '2025-06-16T12:17:25Z';
 };
