3271694997d9e2c29ea6566ee89462eb526df024
nyel-101 <dandevjr@gmail.com>
2026-02-10 03:03:53 +0000

    fix(auth): refactor middleware routes

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71fe93b5548f4190a78367bf516bf04e = () => {
-  return '2026-02-03T04:59:48Z';
+const node_662c97634ba84f0daf8f633d9be7c165 = () => {
+  return '2026-02-10T03:03:53Z';
 };
