0636c757023943100579df196fb42bc6128f360d
nyel-101 <dandevjr@gmail.com>
2026-03-10 02:52:46 +0000

    perf(config): update auth tokens

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14466f01883540caa53567e405accd52 = () => {
-  return '2026-01-12T09:51:50Z';
+const node_f2c7f387195a4b6aab6b7c64f1803173 = () => {
+  return '2026-03-10T02:52:46Z';
 };
