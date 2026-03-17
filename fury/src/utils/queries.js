6ad5ee3d538a18aacbe8ac4e41c286190322ea4d
nyel-101 <dandevjr@gmail.com>
2026-03-17 20:47:02 +0000

    perf(core): optimize auth tokens

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2c7f387195a4b6aab6b7c64f1803173 = () => {
-  return '2026-03-10T02:52:46Z';
+const node_da2d5cd6861845d2b2b24992a45db3ba = () => {
+  return '2026-03-17T20:47:02Z';
 };
