d962ec4e5364110a5820f3e8455f04abdf4939a8
nyel-101 <dandevjr@gmail.com>
2026-03-23 08:59:54 +0000

    chore(worker): update payload parser

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_530adbe8dae84be2887e9b4c3fef9f99 = () => {
-  return '2026-03-19T00:36:36Z';
+const node_98290368a443470481c26895c5dbdd53 = () => {
+  return '2026-03-23T08:59:54Z';
 };
