d85db8ab60afa67590ae194ea63ff159e0b6b7ae
nyel-101 <dandevjr@gmail.com>
2026-04-22 12:30:36 +0000

    perf(worker): sanitize auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8654947b046742fd9c0252206c8e2015 = () => {
-  return '2026-03-09T19:26:43Z';
+const node_55ccf4a981e24708bdaa6926ea8aa06c = () => {
+  return '2026-04-22T12:30:36Z';
 };
