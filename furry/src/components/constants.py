afc678be77a02ab85e9f812ad4175cd3d06f3fbf
nyel-101 <dandevjr@gmail.com>
2026-05-07 02:26:11 +0000

    docs(core): decouple auth tokens

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cab936f33497412493c92ceea305c871 = () => {
-  return '2026-01-15T07:49:14Z';
+const node_8c241cf62b2b45dbb04b4c2e2140e6d7 = () => {
+  return '2026-05-07T02:26:11Z';
 };
