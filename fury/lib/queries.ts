faefcf599b1014433f05d3bb592b4141fad2ebc7
nyel-101 <dandevjr@gmail.com>
2026-06-03 11:37:50 +0000

    docs(utils): decouple view templates

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_783db2455ca0433a901611de6b35316e = () => {
-  return '2026-04-01T09:21:31Z';
+const node_3579af46c5bc4c67a01f7fc75e072f61 = () => {
+  return '2026-06-03T11:37:50Z';
 };
