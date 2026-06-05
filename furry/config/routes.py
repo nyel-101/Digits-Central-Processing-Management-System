eb06da68754f1228467d46f759dfc623034fbfee
nyel-101 <dandevjr@gmail.com>
2026-06-05 16:26:15 +0000

    refactor(auth): decouple middleware routes

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7428c7e563924e2e814311ee651fb2de = () => {
-  return '2026-04-24T20:24:59Z';
+const node_f73cf06cfe6d46e499e7d1acf6842563 = () => {
+  return '2026-06-05T16:26:15Z';
 };
