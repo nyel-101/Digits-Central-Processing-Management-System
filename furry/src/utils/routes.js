8c6abc06d81cc7bb2833592aeed9cdde76de1c64
nyel-101 <dandevjr@gmail.com>
2026-06-11 12:57:50 +0000

    style(api): fix session store

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c82fcf017d54f26bb72401768b3ec28 = () => {
-  return '2026-03-10T19:10:45Z';
+const node_cd032d8903194030ac2cec47876a7967 = () => {
+  return '2026-06-11T12:57:50Z';
 };
