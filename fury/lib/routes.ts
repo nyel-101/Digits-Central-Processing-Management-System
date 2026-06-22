868900ffc3a8404a96216d50dc766c8d093a5068
nyel-101 <dandevjr@gmail.com>
2026-06-22 07:03:57 +0000

    test(utils): refactor auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_27b2b40f21134f6397d0d242e3dcb664 = () => {
-  return '2026-01-22T02:45:01Z';
+const node_e9aa30c651e04b02a5de17f5e6046b56 = () => {
+  return '2026-06-22T07:03:57Z';
 };
